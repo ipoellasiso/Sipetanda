8c44108b0608bdc0a1287229f11e8ae520c204b2
ipoellasiso <ipoellasiso86@gmail.com>
2025-04-28 12:25:31 +0800

    up1

--- app/Http/Controllers/Realisasi_HD_Controller.php
+++ app/Http/Controllers/Realisasi_HD_Controller.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\DB;
 
 class Realisasi_HD_Controller extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     // Get data
     public function index(Request $request)
